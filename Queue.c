c515eb16e72f0ff461bcd46041a6873accefa72c
Anchal Rathore <76997901+anchal0404@users.noreply.github.com>
2023-10-14 13:40:39 +0530

    Update Queue.c

--- Queue.c
+++ Queue.c
@@ -1,133 +1,122 @@
-#include <stdio.h>
+#include<stdio.h>
 #include<stdlib.h>
+#ifndef SUCCESS
+#define SUCCESS 1
+#define FAILURE 0
+#endif
 
-#define FAILURE -1
-#define SUCCESS 0
-#define Q_FULL -1
-typedef struct {
+typedef struct
+{
 	unsigned int capacity;
-	int front,rear;
-	int* items;
+	int front, rear;
+	int *items;
 }Queue_t;
 
-int creat_Queue(Queue_t* q,int size)
+int isFULL(Queue_t* Q)
 {
-	q->items = malloc(sizeof(int)*size);
-	if(q->items == NULL)
+	if(Q->rear == Q->capacity - 1)
+		return 1;
+	return 0;
+}
+
+int isEmpty(Queue_t* Q)
+{
+	if(Q -> rear==-1)
+		return 1;
+	return 0;
+}
+int create_queue(Queue_t *Q,int size)
+{
+	Q->items = (int*)malloc(sizeof(int)*size);
+	Q->capacity=size;
+	Q->front=Q->rear=-1;
+	return SUCCESS;
+}
+
+int enqueue(Queue_t* Q,int data)
+{
+	if(isFULL(Q))
 		return FAILURE;
-	q->rear = -1;
-	q->front = -1;
-	q->capacity = size;
-	return SUCCESS;
-}
-
-int IsQueueFull(Queue_t *q)
-{
-	if(q->rear==(q->capacity)-1)
-		return 1;
 	else
-		return 0;
+		++Q->rear;
+		Q->items[Q->rear]=data;
+		return SUCCESS;
 }
 
-int IsQueueEmpty(Queue_t *q)
+int dequeue(Queue_t* Q)
 {
-	if(q->rear==-1)
-		return 1;
-	else
-		return 0;
-}
-
-/* Function to Insert the element */
-int enqueue(Queue_t *q, int data)
-{
-	if(IsQueueFull)
-		return Q_FULL;
-	if(q->front==-1)
-		q->front+=1;
-	++(q->rear);
-	q->items[q->rear]=data;
-	return SUCCESS;
+	int element;
+	if(isEmpty(Q))
+		return FAILURE;
+	Q->front++;
+	element = Q->items[Q->front];
+	return element;
 }
 
 
-
-/* Function to print the Queue */
-int print_queue(Queue_t q)
+int print_queue(Queue_t *Q)
 {
-
-	/* Checking the queue is empty or not */
-	if (IsQueueEmpty(&q) == SUCCESS)
+	if(isEmpty)
 	{
-		printf("INFO : Queue is empty\n");
-		return FAILURE;
+		printf("INFO:Queue is empty\n");
+	}
+	else
+	{
+		printf("FRONT->");
+
+		while(Q->front)
+		{
+			printf("%d ",Q->items[Q->front]);
+			Q->front = Q->front + 1;
+		}
+		printf("<-REAR\n");
+		return SUCCESS;
 	}
 
-	printf("Front -> ");
-	do
-	{
-		printf("%d ", q.items[q.front]);
-		q.front = (q.front + 1) % q.capacity;
-		/* Printing the queue till it will reaches end */	
-	}while (--(q.front));
-
-	printf("<- Rear\n");
-
-	/* Returning SUCCESS */
-	return SUCCESS;
 }
 
 int main(int argc, char const *argv[])
 {
-	Queue_t q;
-	int size;
+	Queue_t Q;
+	int size,choice;
 	printf("Enter the size of the queue: ");
 	scanf("%d",&size);
 
-	if(creat_Queue(&q,size)==FAILURE)
-	{
-		printf("INFO : Queue not created\n");
-		return FAILURE;
-	}
-	
-	char ch;
-	int choice, data;
-	printf("1. Enqueue\n2. Dequeue\n3. Print Queue\n4. Exit\nEnter the option : ");
-	while (1)
-	{
-		scanf("%d", &choice);
+	create_queue(&Q,size);
 
+	while(1)
+	{
+		printf("\nEnter your choice:\n");
+		
+		printf("1.Enqueue\n2.Dequeue\n3.Print queue\n4.Exit\n");
+		scanf("%d",&choice);
 		switch(choice)
 		{
 			case 1:
+				int data;
 				/* Function to Enqueue the Queue */
 				printf("Enter the element you want to insert : ");
 				scanf("%d", &data);
-				if (enqueue(&q, data) == FAILURE)
+				if (enqueue(&Q, data) == FAILURE)
 				{
 					printf("INFO : Queue full\n");
 				}
 				break;
-			/*case 2:
-				//Function to dequeueue the queue 
-				if (dequeue(&q) == FAILURE)
-				{
-					printf("INFO : Queue is empty\n");
-				}
-				else
-				{
-					printf("INFO : Dequeue successfull\n");
-				}
-				break;*/
 			case 3:
 				/* Function to print the queue */
-				print_queue(q);
+				print_queue(&Q);
 				break;
 			case 4:
-				return SUCCESS;
+				exit(1);
 			default:
-				printf("Invalid option !!!\n");
+				printf("Invalid option.\n");
+
 		}
 
+
 	}
-}
 	
+
+	
+	return 0;
+}
